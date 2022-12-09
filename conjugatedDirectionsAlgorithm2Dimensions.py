5f97753b2208d50117e46d03fe6c74c9f9aa65e9
ffp1 <120139987+ffp1@users.noreply.github.com>
2022-12-09 16:16:43 +0100

    Update conjugatedDirectionsAlgorithm2Dimensions.py

--- conjugatedDirectionsAlgorithm2Dimensions.py
+++ conjugatedDirectionsAlgorithm2Dimensions.py
@@ -43,27 +43,6 @@ d0=[-g0[0],-g0[1]]
 #at this point we know that an x* critical point exists in R^2 and it's the only point of minimum 
 #since Q determinant is defined positive in the quadratic form (function is strictly convex)
 
-#Demonstration:
-
-#let B a basis in R^n s.t. B={d0,d1,...,dn-1}, in this case we define a basis in R^2 B={d0,d1}
-#we also know that exist n coefficients bi in R s.t. x*=b0*d0+b1*d1+...+bn-1*dn-1 so for each iteration k
-#we can multiply by di^t*Q each member and obtain di^t*Q*x*=bi*di^t*Q*di-->bi=(di^t*Q*x*)/di^tQdi
-#this because by hypothesis we are assuming di*Q*dj=0 for each i!=j
-
-#we also know that x* in a quadratic form is x*=-Q^-1c so definitely we have:
-#bi=-(di^t*c )/di^tQdi
-
-#So we can write in the general form x*=x0+sum|i=0-->n-1 of -di^t*c*di/di^t*Q*di , x0 in R^2 initial starting point
-#since our problem x*=alfa0d0+alfa1d1+...+alfan-1dn-1
-#we can define (after eliminating i!=j components premultiplicating for di^t*Q in the k iteration) alfak=di^t*Q*x*/di^t*Q*di
-#since x*=-Q^-1*c--> alfak=dk^t*c/dk^t*Q*dk with alfak being the optimal step from the point xk towards the direction dk
-
-#We can now redefine our problem x*=sum|i=0-->d-1 of dk^t*c*dk/dk^t*Q*dk and xk+1=xk+alfak*dk
-
-#dk+1 will be calculated s.t. dk^t*Q*dk+1=0 taken d0 as initial direction
-
-#At this point we have all the variables needed to implement the algorithm:
-
 #1) calculate dk+1 direction given dk and Q (already multiplied)
 
 def calculateDirectionMethodD1(d0Q):
