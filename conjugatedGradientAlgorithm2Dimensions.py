4c909511f6eb197138b14ed2a3a1bdfbdea3c609
ffp1 <120139987+ffp1@users.noreply.github.com>
2022-12-09 16:15:12 +0100

    Update conjugatedGradientAlgorithm2Dimensions.py

--- conjugatedGradientAlgorithm2Dimensions.py
+++ conjugatedGradientAlgorithm2Dimensions.py
@@ -3,15 +3,7 @@
 #The script is an algorithm that works in 2 variables quadratic functions to find the global minimum
 #using the conjugated Gradient method
 
-#we can extend this algorithm to n dimensions but alfak step will need to be calculated with armijo rule
-#and we can use  Fletcher-Reeves formula to calculate bk+1= norm(gk+1)/norm(gk)
-
 #link to the article https://www.stat.cmu.edu/~ryantibs/convexopt-F13/scribes/lec10.pdf
-
-#Notation:
-#^t indicates the transposed operator in arrays
-#* indicates multiplication sign except in x* indicating a point in R^2
-#sum|i=c1-->c2 of f(i) indicates summation from c1 to c2 in the input function f(i)
 
 import numpy
 import math
@@ -42,10 +34,6 @@ d0=[-g0[0],-g0[1]]
 #at this point we know that an x* critical point exists in R^2 and it's the only point of minimum 
 #since Q determinant is defined positive in the quadratic form (function is strictly convex)
 
-#the conjugated gradient method differs from the conjugated directions in the choiche of d0 , and dk+1
-# to calculate dk+1 we'll have to introduce a new variable bk+1=gk+1^t*Q*dk/dk^t*Q*dk , gk is the gradient
-#calculate at xk, the next direction dk+1 is a perturbation of the antigradient in xk+1 obtained in the following way
-# dk+1=-gk+1+bk+1*dk
 
 #1) calculate dk+1 direction given dk and Q (already multiplied)
 
