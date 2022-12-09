468a5be31afafb67a9d031196a83bc460e012691
ffp1 <120139987+ffp1@users.noreply.github.com>
2022-12-09 22:38:58 +0100

    Update conjugatedGradientAlgorithm2Dimensions.py

--- conjugatedGradientAlgorithm2Dimensions.py
+++ conjugatedGradientAlgorithm2Dimensions.py
@@ -3,6 +3,8 @@
 #The script is an algorithm that works in 2 variables quadratic functions to find the global minimum
 #using the conjugated Gradient method
 
+#References: https://www.stat.cmu.edu/~ryantibs/convexopt-F13/scribes/lec10.pdf
+
 #Notation:
 #^t indicates the transposed operator in arrays
 #* indicates multiplication sign except in x* indicating a point in R^2
