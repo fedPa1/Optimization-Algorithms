bb76c4c786a4100ffdd4f9c22b651b8666aedf67
Federico Pantini <120139987+fedPa1@users.noreply.github.com>
2022-12-14 19:51:31 +0100

    Update conjugatedDirectionsAlgorithm2Dimensions.py

--- Conjugated Direction Algorithm/conjugatedDirectionsAlgorithm2Dimensions.py
+++ Conjugated Direction Algorithm/conjugatedDirectionsAlgorithm2Dimensions.py
@@ -3,7 +3,7 @@
 
 #The script is an algorithm that works in 2 variables quadratic functions to find the global minimum
 #using a variation created by me of conjugated directions method, the algorithm seems to be working , however it's not broadly tested
-#or demonstrated and it was purely done for fun, the main difference is that we can calculate a direction d1 given as initial direction the antigradient
+#or demonstrated and it was purely done for fun, the difference is that we can calculate a direction d1 given as initial direction the antigradient
 #iteratively avoiding to initially set a Basis in R^n of Q-conjugated directions.
 
 #References: https://www.stat.cmu.edu/~ryantibs/convexopt-F13/scribes/lec10.pdf
