8cf79a026885e392937c4f275ff6bffb348d285b
Federico Pantini <120139987+fedPa1@users.noreply.github.com>
2022-12-15 16:22:23 +0100

    Update gradientMethodAlgorithm.py

--- Gradient Method Algorithm/gradientMethodAlgorithm.py
+++ Gradient Method Algorithm/gradientMethodAlgorithm.py
@@ -10,20 +10,11 @@ import armijoRule
 
 x=[random.random()*1000,random.random()*1000]
 
-#function (f0 function,g0 gradient,h0 hessian matrix) f0 must match 1/2*x^tQx+c^tx form
-#we can input any function as long it matches the required form with Q determinant defined positive
+#function (f0 function,g0 gradient)
+#we can input any 2 variables function
 
 f0=5*x[0]**2-2*x[0]+5*x[1]+3*x[1]**2-2*x[0]*x[1] 
 g0=[10*x[0]-2*x[1]-2,6*x[1]-2*x[0]+5]
-#h0=[[10,-2],[-2,6]]
-
-#we will need to express the function in a quadratic form 1/2*x^tQx+c^tx
-#so we declare 2 arrays composed by c (2x1) and Q coefficients (2x2)
-c=[-2,5]
-Q=[[10,-4],[0,6]]
-
-#d0 taken using the antigradient
-d0=[-g0[0],-g0[1]]
 
 k=0
 while (k<=10**4):
@@ -34,7 +25,7 @@ while (k<=10**4):
                                         #Generally we must put a certain tolerance to the gradient tending towards 0
         print("global minimum at",x,"after",k,"iterations")
         break
-    d0=[-g0[0],-g0[1]]
+    d0=[-g0[0],-g0[1]] # we always use the antigradient as direction dk
     alfak=armijoRule.armijo(f0, x, d0, g0) #we use armijo to calculate the alphak step
-    x=[x[0]+alfak*d0[0],x[1]+alfak*d0[1]]
+    x=[x[0]+alfak*d0[0],x[1]+alfak*d0[1]] #new xk+1 point
     k+=1
