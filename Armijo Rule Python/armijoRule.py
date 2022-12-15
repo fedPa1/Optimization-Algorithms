d94ce66b3f0347ae6a649cd41da0244b4065c103
Federico Pantini <120139987+fedPa1@users.noreply.github.com>
2022-12-15 13:06:10 +0100

    Update armijoRule.py

--- Armijo Rule Python/armijoRule.py
+++ Armijo Rule Python/armijoRule.py
@@ -1,5 +1,6 @@
-# @Author: Federico Pantini
-# Contact: fedpa35@gmail.com
+# @Author: Federico Pantini 
+
+# email:fedpa35@gmail.com
 
 # The script computes an alpha step that minimizes the given 2 variables function using the
 # armijo's rule
@@ -8,26 +9,18 @@ import numpy
 import math
 import random
 
-x=[random.random()*1000,random.random()*1000] #initial random point
+x=[random.random()*100,random.random()*100] #initial random point
 
 #we can input any 2 variable function,gradient,direction and point as long they match this format:
 f0=5*x[0]**2-2*x[0]+5*x[1]+3*x[1]**2-2*x[0]*x[1] 
 g0=[10*x[0]-2*x[1]-2,6*x[1]-2*x[0]+5]
-d0=[-g0[0],-g0[1]] #d0 must be descent to guarantee that an alpha step will be found
+#d0=[-g0[0],-g0[1]] #d0 must be descent to guarantee that an alpha step will be found
+d0=[-g0[0],-g0[1]]
 
 
-class armijoClass:
-    def __init__(self,f,x,d,g0):#takes as input a 2 variables function f(x[0],x[1]) ,x s.t. x=[x[0],x[1]]
-                                # d s.t. d=[d[0],d[1]],g0 s.t. g0=[g0[0],g0[1]]
-        self.f=f
-        self.x=x
-        self.d=d
-        self.g0=g0
-    def armijoRule(self): 
-        f=self.f
-        x=self.x
-        d=self.d
-        g0=self.g0
+def armijo(f,x,d,g0): 
+        print(x,d,numpy.dot(g0,d),numpy.linalg.norm(g0)**2)
+        print(1.e-3*numpy.linalg.norm(g0))
         if(numpy.dot(g0,d)>=0):
             print("initial direction not descent, please choose a valid direction")
             return
@@ -43,5 +36,4 @@ class armijoClass:
                 break
             alfa=alfa*delta
             j+=1
-armijoC=armijoClass(f0, x, d0, g0) 
-alfa=armijoClass.armijoRule(armijoC)
+alfa=armijo(f0,x,d0,g0)
