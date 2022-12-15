f023bc1655501038e5085dc91f8bfa61272fa856
Federico Pantini <120139987+fedPa1@users.noreply.github.com>
2022-12-15 12:58:04 +0100

    Update conjugatedGradientAlgorithm2Dimensions.py

--- Conjugated Gradient Algorithm/conjugatedGradientAlgorithm2Dimensions.py
+++ Conjugated Gradient Algorithm/conjugatedGradientAlgorithm2Dimensions.py
@@ -28,28 +28,23 @@ g0=[10*x[0]-2*x[1]-2,6*x[1]-2*x[0]+5]
 c=[-2,5]
 Q=[[10,-2],[-2,6]]
 
-#d0 taken using the antigradient
-d0=[-g0[0],-g0[1]]
-
 #at this point we know that an x* critical point exists in R^2 and it's the only point of minimum 
 #since Q determinant is defined positive in the quadratic form (function is strictly convex)
 
-#we now have all the data needed to start the algorithm and can procede with the iteration
+#d0 taken using the antigradient
+d0=[-g0[0],-g0[1]]
 
 k=0
-while (k<=10**4):
-    g0=[10*x[0]-2*x[1]-2,6*x[1]-2*x[0]+5]
-    d0Q=numpy.dot(d0,Q) 
-    print("k=",k,"Gradient",numpy.linalg.norm(g0),"point",x)
+while (k<=10):
     if (numpy.linalg.norm(g0)<=1.e-7): #algorithm found the only critical point and must be global in the strictly convex function
                                    #you might want to set an arbitrary tolerance eps>0 s.t. norm(g0)<=eps since there can be cases where
                                    # norm(g0) will never converge to 0
         print("global minimum at",x,"after",k,"iterations")
         break
-    alfak=-numpy.dot(g0,d0)/numpy.dot(d0Q,d0) #calculates alfak step to take from point xk
-    x=[x[0]+alfak*d0[0],x[1]+alfak*d0[1]]
-    b1=numpy.dot(g0,d0Q)/numpy.dot(d0Q,d0)
-    d0=[g0[0]+numpy.dot(b1,d0)[0],g0[1]+numpy.dot(b1,d0)[1]]
+    d0Q=numpy.dot(d0,Q) 
+    alfak=-numpy.dot(g0,d0)/numpy.dot(d0Q,d0)
+    x=[x[0]+alfak*d0[0],x[1]+alfak*d0[1]] 
+    g0=[numpy.dot(Q,x)[0]+c[0],numpy.dot(Q,x)[1]+c[1]] 
+    b1=numpy.dot(g0,d0Q)/numpy.dot(d0Q,d0) 
+    d0=[-g0[0]+numpy.dot(b1,d0)[0],-g0[1]+numpy.dot(b1,d0)[1]]
     k+=1
-
-
