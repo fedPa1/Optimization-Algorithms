319e578b3550619fed3c9d7cf08a473cd9fb51ab
Federico Pantini <120139987+fedPa1@users.noreply.github.com>
2022-12-16 16:51:27 +0100

    Update testSimpleOptimization.py

--- Simple Alpha Step/testSimpleOptimization.py
+++ Simple Alpha Step/testSimpleOptimization.py
@@ -10,19 +10,20 @@ import random
 
 def simpleAlphaStep(f,g,x,d):
     alfa=random.random()*100 #alfa0 step >0
-    fAlfa=5*(x[0]+alfa*d[0])**2-2*(x[0]+alfa*d[0])+5*(x[1]+alfa*d[1])+3*(x[1]+alfa*d[1])**2-2*(x[0]+alfa*d[0])*(x[1]+alfa*d[1])
+    #fAlfa=#your f(x+alpha*d) function
     j=0
     if(numpy.dot(g,d)>=0):
             print("initial direction not descent, please choose a valid direction")
             return
     while(j<=10**5):
-        fAlfa=5*(x[0]+alfa*d[0])**2-2*(x[0]+alfa*d[0])+5*(x[1]+alfa*d[1])+3*(x[1]+alfa*d[1])**2-2*(x[0]+alfa*d[0])*(x[1]+alfa*d[1])
+        fAlfa=#your f(x+alpha*d) function
         if (fAlfa<=f):
             print("after",j,"iterations found",alfa)
             return alfa
         alfa*=0.5
         j+=1
     return
+
 x=[random.random()*1000,random.random()*1000]
 f0=5*x[0]**2-2*x[0]+5*x[1]+3*x[1]**2-2*x[0]*x[1] 
 g0=[10*x[0]-2*x[1]-2,6*x[1]-2*x[0]+5]
