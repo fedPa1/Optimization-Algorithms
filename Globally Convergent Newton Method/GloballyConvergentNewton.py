fedfe15cb5866f33fc258357e27a65dfcff19efa
Federico Pantini <120139987+fedPa1@users.noreply.github.com>
2022-12-16 18:14:07 +0100

    Add files via upload

---
+++ Globally Convergent Newton Method/GloballyConvergentNewton.py
@@ -0,0 +1,63 @@
+# @Author: Federico Pantini 
+# email:fedpa35@gmail.com
+
+# The script computes the global minimum point of a 2 variable function (that admits a global minimum) using a modification of the Newton Method
+
+
+import numpy
+import random
+import armijoRule
+
+#initial random point x in R^2 
+
+x=[random.random()*1000,random.random()*1000]
+
+#function (f0 function,g0 gradient,h0 hessian matrix), we can input any 2 variables function
+
+f0=5*x[0]**2-2*x[0]+5*x[1]+3*x[1]**2-2*x[0]*x[1] 
+g0=[10*x[0]-2*x[1]-2,6*x[1]-2*x[0]+5]  #your input function,Q,g0,d0
+d0=[-g0[0],-g0[1]]
+Q=[[10,-2],[-2,6]]
+alfak=0
+sk=[0,0]
+
+k=0
+while k<=10**7:
+    f0=5*x[0]**2-2*x[0]+5*x[1]+3*x[1]**2-2*x[0]*x[1] 
+    g0=[10*x[0]-2*x[1]-2,6*x[1]-2*x[0]+5]
+    if (numpy.linalg.norm(g0)<=1.e-6):
+        print("global minimum at",x,"after",k,"iterations")
+        break
+    DetQ=Q[0][0]*Q[1][1]-Q[0][1]*Q[1][0]
+
+    c1=0.000001 #arbitrary constant >0 we set the degree of tolerance choosing Newton method over the antigradient
+         # to calculate next direction, low c1 => more iterations with Newton method
+         #you can play around yourself with c1,c2 constants to see different results!
+
+    if (abs(DetQ)>c1*numpy.linalg.norm(g0)**2): #DetQ must be !=0 
+
+        sk=[-numpy.dot(numpy.linalg.inv(Q),g0)[0],-numpy.dot(numpy.linalg.inv(Q),g0)[1]]
+
+        c2=0.00000001 #arbitrary constant >0 , this sets how much the direction should be descending ,
+             # low c2 => more tolerance towards less descending direction
+
+        if(numpy.dot(g0,sk)<=-c2*numpy.linalg.norm(g0)**2):
+            print("Newthon method used")
+            d0=[sk[0],sk[1]]
+            x=[x[0]+d0[0],x[1]+d0[1]] #its x+d0 because we already put a minus in sk (xk+1=xk-g0/Q)
+        
+        #the direction sk couldn't match expectations so we use antigradient path
+        else:
+            print("Gradient method used")
+            d0=[-g0[0],-g0[1]]
+            alfak=armijoRule.armijo(f0,x,d0,g0)
+            x=[x[0]+alfak*d0[0],x[1]+alfak*d0[1]]
+
+    #the direction sk couldn't match expectations so we use antigradient path
+    else:
+        print("Gradient method used")
+        d0=[-g0[0],-g0[1]]
+        alfak=armijoRule.armijo(f0,x,d0,g0)
+        x=[x[0]+alfak*d0[0],x[1]+alfak*d0[1]]
+    k+=1
+    print(x)
---
+++ Globally Convergent Newton Method/armijoRule.py
@@ -0,0 +1,28 @@
+# @Author: Federico Pantini 
+
+# email:fedpa35@gmail.com
+
+# The script computes an alpha step that minimizes the given 2 variables function using the
+# armijo's rule
+
+import numpy 
+import math
+import random
+
+def armijo(f,x,d,g): 
+        if(numpy.dot(g,d)>=0):
+            print("initial direction not descent, please choose a valid direction")
+            return
+        alpha=1 #alfa0 step >0
+        delta=0.5 # 0<delta<1
+        gamma=0.5 # 0<gamma<=0.5
+        j=0
+        while(j<=10**4):
+            #fAlpha=  your f(x+alpha*d) function
+            fAlpha=5*(x[0]+alpha*d[0])**2-2*(x[0]+alpha*d[0])+5*(x[1]+alpha*d[1])+3*(x[1]+alpha*d[1])**2-2*(x[0]+alpha*d[0])*(x[1]+alpha*d[1])
+            if (fAlpha<=f+gamma*alpha*numpy.dot(g,d)):
+               # print("after",j,"iterations found","alpha=",alpha)
+                return alpha
+                break
+            alpha*=delta
+            j+=1
