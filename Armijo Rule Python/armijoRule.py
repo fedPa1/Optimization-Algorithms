fdc8e7debcf978515550772a1166d6f7b8391f33
Federico Pantini <120139987+fedPa1@users.noreply.github.com>
2022-12-16 16:47:43 +0100

    Update armijoRule.py

--- Armijo Rule Python/armijoRule.py
+++ Armijo Rule Python/armijoRule.py
@@ -9,29 +9,23 @@ import numpy
 import math
 import random
 
-x=[random.random()*100,random.random()*100] #initial random point
-
-#we can input any 2 variable function,gradient,direction and point as long they match this format:
-f0=5*x[0]**2-2*x[0]+5*x[1]+3*x[1]**2-2*x[0]*x[1] 
-g0=[10*x[0]-2*x[1]-2,6*x[1]-2*x[0]+5]
-#d0=[-g0[0],-g0[1]] #d0 must be descent to guarantee that an alpha step will be found
-d0=[-g0[0],-g0[1]]
-
-
-def armijo(f,x,d,g0): 
-        if(numpy.dot(g0,d)>=0):
+def armijo(f,x,d,g): 
+        f0=f
+        x0=x
+        g0=g
+        d0=d
+        if(numpy.dot(g0,d0)>=0):
             print("initial direction not descent, please choose a valid direction")
             return
-        alfa=1 #alfa0 step >0
+        alpha=1 #alfa0 step >0
         delta=0.5 # 0<delta<1
         gamma=0.5 # 0<gamma<=0.5
         j=0
-        while(j<=10**5):
-            fAlfa=5*(x[0]+alfa*d[0])**2-2*(x[0]+alfa*d[0])+5*(x[1]+alfa*d[1])+3*(x[1]+alfa*d[1])**2-2*(x[0]+alfa*d[0])*(x[1]+alfa*d[1])
-            if (fAlfa<=f+gamma*alfa*numpy.dot(g0,d)):
-                print("after",j,"iterations found","alfa=",alfa)
-                return alfa
+        while(j<=10**4):
+            #fAlpha=  your f(x+alpha*d) function
+            if (fAlpha<=f0+gamma*alpha*numpy.dot(g0,d0)):
+                print("after",j,"iterations found","alpha=",alpha)
+                return alpha
                 break
-            alfa=alfa*delta
+            alpha*=delta
             j+=1
-alfa=armijo(f0,x,d0,g0)
