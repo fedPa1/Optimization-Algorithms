c7d12aaffb9adc2c4f09972523de41a636c75d4c
Federico Pantini <120139987+fedPa1@users.noreply.github.com>
2022-12-16 16:51:47 +0100

    Update testSimpleOptimization.py

--- Simple Alpha Step/testSimpleOptimization.py
+++ Simple Alpha Step/testSimpleOptimization.py
@@ -25,7 +25,7 @@ def simpleAlphaStep(f,g,x,d):
     return
 
 x=[random.random()*1000,random.random()*1000]
-f0=5*x[0]**2-2*x[0]+5*x[1]+3*x[1]**2-2*x[0]*x[1] 
+f0=5*x[0]**2-2*x[0]+5*x[1]+3*x[1]**2-2*x[0]*x[1]  #your function , x point etc..
 g0=[10*x[0]-2*x[1]-2,6*x[1]-2*x[0]+5]
 d0=[-g0[0],-g0[1]]
 simpleAlphaStep(f0,g0,x,d0)
