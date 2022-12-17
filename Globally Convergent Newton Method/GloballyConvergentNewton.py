d4cd2108153edfb409a1f8fea65c0529d68ea3ce
Federico Pantini <120139987+fedPa1@users.noreply.github.com>
2022-12-17 21:20:33 +0100

    Update GloballyConvergentNewton.py

--- Globally Convergent Newton Method/GloballyConvergentNewton.py
+++ Globally Convergent Newton Method/GloballyConvergentNewton.py
@@ -25,39 +25,18 @@ k=0
 
 print("Initial point",x)
 
-while k<=10**7:
+while k<=10**5:
     f0=5*x[0]**2-2*x[0]+5*x[1]+3*x[1]**2-2*x[0]*x[1] 
     g0=[10*x[0]-2*x[1]-2,6*x[1]-2*x[0]+5]
     if (numpy.linalg.norm(g0)<=1.e-6):
         print("global minimum at",x,"after",k,"iterations")
         break
     DetQ=Q[0][0]*Q[1][1]-Q[0][1]*Q[1][0]
-
-    c1=1.e-7 #arbitrary constant >0 we set the degree of tolerance choosing Newton method over the antigradient
-         # to calculate next direction, low c1 => more iterations with Newton method
-         #you can play around yourself with c1,c2 constants to see different results
-
-    if (abs(DetQ)>c1*numpy.linalg.norm(g0)**2): #DetQ must be !=0 
-
+    if (DetQ!=0): #DetQ must be !=0 to invert the matrix
         sk=[-numpy.dot(numpy.linalg.inv(Q),g0)[0],-numpy.dot(numpy.linalg.inv(Q),g0)[1]]
-
-        c2=1.e-7 #arbitrary constant >0 , this sets how much the direction should be descending ,
-             # low c2 => more tolerance towards less descending direction, however a low tolerance doesn't mean
-             # that it will need less iterations, it is used primarily to avoid potential too weak directions calculated
-             #in the next alphak step
-
-        if(numpy.dot(g0,sk)<=-c2*numpy.linalg.norm(g0)**2):
-            print("Newthon method used")
-            d0=[sk[0],sk[1]]
-            x=[x[0]+d0[0],x[1]+d0[1]] #its x+d0 because we already put a minus in sk (xk+1=xk-g0/Q)
-        
-        #the direction sk couldn't match expectations so we use antigradient path
-        else:
-            print("Gradient method used")
-            d0=[-g0[0],-g0[1]]
-            alfak=armijoRule.armijo(f0,x,d0,g0)
-            x=[x[0]+alfak*d0[0],x[1]+alfak*d0[1]]
-
+        print("Newthon method used")
+        d0=[sk[0],sk[1]]
+        x=[x[0]+d0[0],x[1]+d0[1]] 
     #the direction sk couldn't match expectations so we use antigradient path
     else:
         print("Gradient method used")
